e05a00d9078f403a31e5852ce0b8043e8bb87b87
Xiaozao Wang <115412906+XiaozaoWang@users.noreply.github.com>
2024-10-20 22:00:56 +0800

    Help button

--- src/components/BlackBox.jsx
+++ src/components/BlackBox.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Handle, Position } from "@xyflow/react";
 import { tw } from "twind";
-import { FaSyncAlt } from "react-icons/fa"; // Import reload icon
+import { FaSyncAlt, FaQuestionCircle } from "react-icons/fa"; // Import reload and question icon
 
 const BlackBox = ({ data, isConnectable }) => {
   const [isGlowing, setIsGlowing] = useState(false); // State for glow effect
@@ -22,6 +22,11 @@ const BlackBox = ({ data, isConnectable }) => {
     setTimeout(() => setIsRotating(false), 500); // Stop rotation after 1 second
   };
 
+  const [isExplanationVisible, setIsExplanationVisible] = useState(false); // State for explanation bubble
+  const toggleExplanation = () => {
+    setIsExplanationVisible((prev) => !prev); // Toggle explanation visibility
+  };
+
   return (
     <>
       <Handle
@@ -31,7 +36,7 @@ const BlackBox = ({ data, isConnectable }) => {
         className={tw`w-2 h-2 bg-blue-400`}
       />
       <div
-        className={tw`w-[${nodeWidth}px] h-[${nodeHeight}px] p-0 m-0 bg-gray-100 border border-gray-300 rounded-md flex flex-col items-center justify-center transition-all duration-500 ${
+        className={tw`w-[${nodeWidth}px] h-[${nodeHeight}px] p-0 m-0 bg-gray-100 border border-gray-300 rounded-md flex flex-col items-center justify-center relative transition-all duration-500 ${
           isGlowing ? "ring-8 ring-yellow-200 ring-opacity-30 blur-sm" : ""
         }`} // Apply glow effect when isGlowing is true
       >
@@ -56,6 +61,32 @@ const BlackBox = ({ data, isConnectable }) => {
         isConnectable={isConnectable}
         className={tw`w-2 h-2 bg-blue-400`}
       />
+      <button
+        onClick={toggleExplanation}
+        className={tw`absolute top-[8px] right-[8px] text-gray-500 hover:text-gray-700`}
+      >
+        <FaQuestionCircle size={18} />
+      </button>
+      {isExplanationVisible && (
+        <div
+          className={tw`absolute top-[20px] left-[${
+            nodeWidth - 8
+          }px] bg-white border border-gray-300 rounded-md p-2 shadow-lg text-xs w-40`} // Positioned top-right relative to the button
+        >
+          <p className={tw`pb-[5px]`}>
+            After you selected the data, click the{" "}
+            <strong className={tw`text-blue-500 font-bold`}>Train</strong>{" "}
+            button to train the neural network.
+          </p>
+          <p>
+            You need to click{" "}
+            <strong className={tw`text-blue-500 font-bold`}>
+              multiple times
+            </strong>{" "}
+            to improve the results.
+          </p>
+        </div>
+      )}
     </>
   );
 };
--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,119 +1,9 @@
-// import React, { useState, useEffect, useRef } from "react";
-// import { tw } from "twind";
-// import * as d3 from "d3";
-
-// const InputField = ({ data }) => {
-//   const svgRef = useRef(null);
-
-//   const nodeWidth = 220;
-//   const nodeHeight = 320;
-//   const margin = { top: 20, bottom: 130, left: 30, right: 20 };
-//   const width = nodeWidth - margin.left - margin.right; // graph width
-//   const height = nodeHeight - margin.top - margin.bottom;
-
-//   const [selectedData, setSelectedData] = useState(data.selectedData); // Track selected circle
-
-//   useEffect(() => {
-//     const svg = d3.select(svgRef.current).attr("cursor", "pointer");
-//     svg.selectAll("*").remove();
-
-//     const graph = svg
-//       .attr("width", nodeWidth)
-//       .attr("height", nodeHeight)
-//       .append("g")
-//       .attr("transform", `translate(${margin.left},${margin.top})`);
-
-//     // Create scales for the x and y axes
-//     const xScale = d3.scaleLinear().domain([-1, 1]).range([0, width]);
-//     const yScale = d3.scaleLinear().domain([-1, 1]).range([height, 0]);
-
-//     // Create the x and y axes
-//     const xAxis = d3.axisBottom(xScale).ticks(5).tickSize(3);
-//     const yAxis = d3.axisLeft(yScale).ticks(5).tickSize(3).tickPadding(2);
-
-//     // Append the x axis to the SVG
-//     graph.append("g").attr("transform", `translate(0,${height})`).call(xAxis);
-
-//     // Append the y axis to the SVG
-//     graph.append("g").call(yAxis);
-
-//     // Draw circles based on passed-in data array
-//     const combinedData = d3.zip(data.inputs, data.targets);
-
-//     graph
-//       .selectAll("circle")
-//       .data(combinedData)
-//       .join("circle")
-//       .attr("cx", (d) => xScale(d[0][0]))
-//       .attr("cy", (d) => yScale(d[0][1]))
-//       .attr("r", 5)
-//       .attr("fill", (d) => (d[1] == 1 ? "blue" : "red"))
-//       .attr("stroke", "none") // Default no stroke
-//       .on("click", function (event, d) {
-//         // Remove any previously added larger circles
-//         console.log("I'm clicked");
-//         graph.selectAll(".selected").remove();
-
-//         // get the index of the selected data
-//         const index = combinedData.findIndex((item) => item === d);
-
-//         console.log("Selected data:", d);
-//         console.log("Selected index:", index);
-//         setSelectedData(index);
-
-//         // Call the onSelect callback to pass selected data to the parent
-//         if (data.onSelect) {
-//           data.onSelect(index);
-//         }
-//       });
-//     graph
-//       .append("circle")
-//       .attr("class", "selected")
-//       .attr("cx", xScale(data.inputs[selectedData][0]))
-//       .attr("cy", yScale(data.inputs[selectedData][1]))
-//       .attr("r", 8) // Larger radius
-//       .attr("fill", "none")
-//       .attr("stroke", "black")
-//       .attr("stroke-width", 2);
-
-//     // add x and y auxiliary lines
-//     graph
-//       .append("line")
-//       .attr("x1", xScale(data.inputs[selectedData][0]))
-//       .attr("y1", yScale(data.inputs[selectedData][1]))
-//       .attr("x2", xScale(data.inputs[selectedData][0]))
-//       .attr("y2", height)
-//       .attr("stroke", "black")
-//       .attr("stroke-width", 1)
-//       .attr("stroke-dasharray", "4");
-
-//     graph
-//       .append("line")
-//       .attr("x1", 0)
-//       .attr("y1", yScale(data.inputs[selectedData][1]))
-//       .attr("x2", xScale(data.inputs[selectedData][0]))
-//       .attr("y2", yScale(data.inputs[selectedData][1]))
-//       .attr("stroke", "black")
-//       .attr("stroke-width", 1)
-//       .attr("stroke-dasharray", "4");
-//   }, [data]);
-
-//   return (
-//     <div
-//       className={tw`w-[${nodeWidth}px] h-[${nodeHeight}px] p-0 m-0 bg-gray-100 border border-gray-300 rounded-md`}
-//     >
-//       <svg ref={svgRef} width="100%" height="100%" />
-//     </div>
-//   );
-// };
-
-// export default InputField;
-
 import React, { useState, useEffect, useRef } from "react";
 import { tw } from "twind";
 import * as d3 from "d3";
 import Samoyed from "../images/samoyed.jpg"; // Ensure you have the correct path
 import Dachshund from "../images/dachshund.jpg"; // Ensure you have the correct path
+import { FaQuestionCircle } from "react-icons/fa";
 
 const InputField = ({ data }) => {
   const svgRef = useRef(null);
@@ -125,6 +15,11 @@ const InputField = ({ data }) => {
   const height = nodeHeight - margin.top - margin.bottom;
 
   const [selectedData, setSelectedData] = useState(data.selectedData); // Track selected circle
+
+  const [isExplanationVisible, setIsExplanationVisible] = useState(false); // State for explanation bubble
+  const toggleExplanation = () => {
+    setIsExplanationVisible((prev) => !prev); // Toggle explanation visibility
+  };
 
   useEffect(() => {
     const svg = d3.select(svgRef.current).attr("cursor", "pointer");
@@ -267,6 +162,40 @@ const InputField = ({ data }) => {
           </p>
         </div>
       </div>
+      <button
+        onClick={toggleExplanation}
+        className={tw`absolute top-[8px] right-[8px] text-gray-500 hover:text-gray-700`}
+      >
+        <FaQuestionCircle size={18} />
+      </button>
+      {isExplanationVisible && (
+        <div
+          className={tw`absolute top-[20px] left-[${
+            nodeWidth - 8
+          }px]  w-48 bg-white border border-gray-300 rounded-md p-2 shadow-lg text-xs`} // Positioned top-right relative to the button
+        >
+          <p className={tw`py-[2px]`}>There are two types of dogs:</p>
+          <ul>
+            <li>
+              <span className={tw`text-blue-500 font-bold`}>Blue</span> points
+              represent Samoyed;
+            </li>
+            <li>
+              <span className={tw`text-red-500 font-bold`}>Red</span> points
+              represent Dachshund.
+            </li>
+          </ul>
+          <p className={tw`py-[0px]`}>
+            <strong>1. </strong>Click on different data points.
+          </p>
+          <p className={tw`py-[0px]`}>
+            <strong>2. </strong>Tell neural network the size and color of a dog.
+          </p>
+          <p className={tw`py-[0px]`}>
+            <strong>3. </strong>See its prediction of the dog's type.
+          </p>
+        </div>
+      )}
     </div>
   );
 };
